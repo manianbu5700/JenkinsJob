9abf8dec2e95678e61b90e5e5dab3cb69ff8ed7f
Manbianbu5700 <manianbu5700@gmail.com>
2026-02-05 23:22:02 +0530

    file added

--- tests/Alert.spec.js
+++ tests/Alert.spec.js
@@ -1,32 +1,32 @@
-import{test,expect} from '@playwright/test'
-test('Alert Functionality',async({page})=>{
+import { test, expect } from '@playwright/test'
+test('Alert Functionality', async ({ page }) => {
     await page.goto('https://demo.automationtesting.in/Alerts.html')
 
-    await page.once('dialog',async(dialog)=>{
-        console.log("Alert Handled:",dialog.message())
+    await page.once('dialog', async (dialog) => {
+        console.log("Alert Handled:", dialog.message())
         await dialog.accept()
     })
     await page.locator('//button[@onclick="alertbox()"]').click()
 
     await page.locator('(//a[@class="analystic"])[2]').click()
 
-    await page.once('dialog',async(dialog)=>{
-        console.log("Confirm Alert :",dialog.message())
+    await page.once('dialog', async (dialog) => {
+        console.log("Confirm Alert :", dialog.message())
         await dialog.accept()
     })
     await page.locator('//button[@onclick="confirmbox()"]').click()
 
     await page.locator('//a[text()="Alert with Textbox "]').click()
 
-    await page.once("dialog",async(dialog)=>{
-        console.log("Prompt Alert:",dialog.message())
-        console.log("Default prompt:",dialog.defaultValue())
-        await dialog.accept('abc')
+    await page.once("dialog", async (dialog) => {
+        console.log("Prompt Alert:", dialog.message())
+        console.log("Default prompt:", dialog.defaultValue())
+        await dialog.accept('manish')
     })
 
     await page.locator('//button[@onclick="promptbox()"]').click()
 
-    const promptmessage=await page.locator('//p[@id="demo1"]')
-    const txt=await promptmessage.textContent()
-    await expect(txt).toContain('abc')
+    const promptmessage = await page.locator('//p[@id="demo1"]')
+    const txt = await promptmessage.textContent()
+    await expect(txt).toContain('manish')
 })
