fada1425ccedf49d189dfc5011d6be2491bb6804
Manbianbu5700 <manianbu5700@gmail.com>
2026-02-06 13:36:02 +0530

    add modified file

--- tests/Alert.spec.js
+++ tests/Alert.spec.js
@@ -21,12 +21,12 @@ test('Alert Functionality', async ({ page }) => {
     await page.once("dialog", async (dialog) => {
         console.log("Prompt Alert:", dialog.message())
         console.log("Default prompt:", dialog.defaultValue())
-        await dialog.accept('Harish')
+        await dialog.accept('mani')
     })
 
     await page.locator('//button[@onclick="promptbox()"]').click()
 
     const promptmessage = await page.locator('//p[@id="demo1"]')
     const txt = await promptmessage.textContent()
-    await expect(txt).toContain('Harish')
+    await expect(txt).toContain('mani')
 })
